8ad510328aae76d3a8b904fff8c7f7567de272a1
Aolith <3775001306@qq.com>
2026-06-08 19:23:48 +0800

    feat: 连接 MongoDB Atlas 数据库成功，排除.env跟踪

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,18 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import express from 'express'
 import cors from 'cors'
-import dotenv from 'dotenv'
+import connectDB from './config/db'
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+connectDB()
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
